676aa6d8daa2c1bedb3322240154ac82d1222181
Zahid <zrashidch@gmail.com>
2017-01-01 23:31:57 -0500

    Initial Checkin of sales tool.

---
+++ SalesTool/src/ca/demo/salestool/Main.java
@@ -0,0 +1,16 @@
+package ca.demo.salestool;
+
+public class Main {
+
+	public static void main(String[] args) {
+		SalesData data = new SalesData();
+		displayGreeting();
+		data.display();
+	}
+		private static void displayGreeting() {
+			System.out.println("Hi this is My first Line");
+			System.out.println("Hi thisis my second line");
+		}
+	}
+
+
---
+++ SalesTool/src/ca/demo/salestool/SalesData.java
@@ -0,0 +1,13 @@
+package ca.demo.salestool;
+
+
+public class SalesData {
+
+	int Data[] = {0, 4, 21};
+	
+	public void display() {
+		
+		System.out.println("Hi this is my salesdata class!: ");
+	}
+	
+}
